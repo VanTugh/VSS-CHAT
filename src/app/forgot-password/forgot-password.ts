46896220cfd89178877d8e6931f350e9f2ce86c2
Nguyễn Văn Tùng <email_github_cua_bro@gmail.com>
2026-08-03 17:13:40 +0700

    update register and forgot password basic

--- src/app/forgot-password/forgot-password.ts
+++ src/app/forgot-password/forgot-password.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.scss',
+  standalone: true
 })
 export class ForgotPassword {}
